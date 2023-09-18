994fbed629f77b6fa40b8067cf96ce6760b04dfa
jlopezru <jlopezru@wellesley.edu>
2023-09-18 13:16:10 -0700

    Handle routing using HashRouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import {Helmet} from 'react-helmet';
 import Navbar from './components/navbar/Navbar';
 import Home from './components/pages/home/Home';
