583344e862b09eff4f93a8341979fb8a68641645
jlopezru <jlopezru@wellesley.edu>
2023-10-30 17:45:41 -0700

    removed react-helmet usage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-import {Helmet} from 'react-helmet';
 import Navbar from './components/navbar/Navbar';
 import Home from './components/pages/home/Home';
 import About from './components/pages/about/About';
@@ -10,9 +9,6 @@ import Footer from './components/footer/Footer';
 
 const App = () => (
   <div className="App">
-    <Helmet>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    </Helmet>
     <Router>
       <Navbar />
       <Routes>
