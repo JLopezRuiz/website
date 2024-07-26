d92c78a1e4ffd634e99db7cbc9209d4e746151f5
JLopezRuiz <jlopezru@wellesley.edu>
2024-07-26 12:12:35 -0700

    improvements for mobile display

--- src/components/resume/ResumeTab.js
+++ src/components/resume/ResumeTab.js
@@ -7,7 +7,7 @@ const ResumeTab = ({title, companyName, description, date }) => {
             <div className={styles['resume-tab-date']}>{date}</div>
             <section className={styles['resume-tab-section']}>
                 <h1>{title}</h1>
-                <h3>{companyName}</h3>
+                <p className={styles['resume-tab-subtitle']}>{companyName}</p>
                 <p>{description}</p>
             </section>
         </div>
